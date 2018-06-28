37c880cce0bd8a06a6aa4f11cdccd9a15b0fc736
David Rapisarda <mridsole@gmail.com>
2018-06-28 19:31:16 +1000

    Attempt at rs-cap
    Working with compression and 16-bit depth image data is too painful,
    need to just save the raw data (for depth) instead.

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
 # Try to capture depth data and color data.
-
-# First import the library
 import pyrealsense2 as rs
 import numpy as np
-import matplotlib.pyplot as plt
-import IPython
+import cv2
 
 # Create a context object. This object owns the 
 # handles to all connected realsense devices.
+
+config = rs.config()
+config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+# config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+# config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
 pipeline = rs.pipeline()
-pipeline.start()
+pipeline.start(config)
 
-# plt.ion()
-
-nx = 1280
-ny = 720
-
-plt.ion()
-
-fig = plt.figure()
-data = np.zeros((ny, nx))
-im = plt.imshow(data, cmap="inferno", vmin=0, vmax=65535/32)
-im.set_data(data)
-fig.canvas.draw()
-plt.show(block=False)
-
-i = 0
 while True:
 
     # This call waits until a new coherent set of frames is available 
@@ -33,23 +22,20 @@ while True:
     frames = pipeline.wait_for_frames()
     depth_fr = frames.get_depth_frame()
     color_fr = frames.get_color_frame()
-    if not depth_fr or not color_fr:
-        continue
+    if not depth_fr or not color_fr: continue
 
     # Get depth and color as numpy arrays.
     depth_data = np.asanyarray(depth_fr.as_frame().get_data())
     color_data = np.asanyarray(color_fr.as_frame().get_data())
 
-    print(depth_data.shape)
-    print(color_data.shape)
+    # Get depth color map
+    import IPython; IPython.embed()
+    depth_data = (2**8) * np.round(depth_data / 2**8)
+    depth_colormap = cv2.applyColorMap(
+        cv2.convertScaleAbs(depth_data, alpha=0.03), 
+        cv2.COLORMAP_JET
+    )
 
-    print(i)
-    i = i + 1
-    # print(data.shape)
-    # print(np.max(data))
-    # print(np.min(data))
-
-    # Plotting
-    # im.set_data(data)
-    # fig.canvas.draw()
-    # plt.show(block=False)
+    cv2.imshow('depth', depth_colormap)
+    cv2.imshow('color', color_data)
+    cv2.waitKey(1)
